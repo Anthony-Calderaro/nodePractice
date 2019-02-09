cb08ec00198a5cab8f72193e20cc417e61321521
Anthony <acalderaro@blueraster.com>
2019-02-09 18:14:06 -0500

    Added other routes.

--- expressRoot/meadowlark.js
+++ expressRoot/meadowlark.js
@@ -6,14 +6,19 @@ const app = express(); // declare an application from express
 
 app.set('port', process.env.POR || 4000); // set the port at whatever the environment variable is or 4000
 
-
+// Add a default path to 'localhost:4000/'. 
 app.get('/', (req, res) => {
   res.type('text/plain');
   res.send('Meadowlark Travel');
 })
 
-// Custom 404 error page
-app.use((req, res) => { 
+app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send('About Meadowlark Travel');
+  
+})
+// Custom 404 error page - all other routes that are not error pages must come before this!
+app.use((req, res) => { // note that errors are app.USE not app.VERB. This is how express handles middleware
   res.type('text/plain');
   res.status(404);
   res.send('404 - Not Found');
