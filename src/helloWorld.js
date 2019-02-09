9885d7d96d76d3d2fff22cf7c78c51a306529023
Anthony <acalderaro@blueraster.com>
2019-02-08 22:46:23 -0500

    Added different responses and content for different client pages.

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,8 +1,24 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!');
+  const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+
+  switch(path) {
+    case '':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Homepage');
+      break;
+
+    case '/about':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('About!');
+      break;
+
+    default:
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('Not Found!');
+      break;
+  }
 }).listen(3000);
 
 console.log('====== Server started on localhost:3000 ====== ');
