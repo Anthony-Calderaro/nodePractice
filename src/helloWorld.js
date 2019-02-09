42e25d7fb7b1407c784edc96e75213bc253abcec
Anthony <acalderaro@blueraster.com>
2019-02-09 17:50:21 -0500

    Chapter 2

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,22 +1,39 @@
 const http = require('http');
+const fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+  if (!responseCode) responseCode = 200;
+
+  fs.readFile(__dirname + path, function(err, data) {
+    console.log(err, data);
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('500 - Internal Error');
+    } else {
+      res.writeHead(responseCode, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+}
+
+
+
+
 
 http.createServer((req, res) => {
   const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
 
   switch(path) {
     case '':
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Homepage');
+      serveStaticFile(res, './public/home.html', 'text/html')
       break;
-
+      
     case '/about':
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('About!');
+      serveStaticFile(res, './public/about.html', 'text/html')
       break;
-
+      
     default:
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('Not Found!');
+      serveStaticFile(res, './public/404.html', 'text/html')
       break;
   }
 }).listen(3000);
